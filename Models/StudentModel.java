0f71dfb7e2f550223a67b28d7ce47ccf920298d2
domakdomak <domagalski.mt@gmail.com>
2017-08-30 15:23:06 +0200

    Add StudentModel with constructor

---
+++ Models/StudentModel.java
@@ -0,0 +1,8 @@
+public class StudentModel extends UserModel {
+
+    public StudentModel(String name, String surname,
+                        String login, String password, String email) {
+
+        super(name, surname, login, password, email);
+    }
+}
