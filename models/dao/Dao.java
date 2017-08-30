042375a41515a5827dade918132b9b3cffec9ef2
Kamil Postrożny <kamilp009@gmail.com>
2017-08-30 16:16:40 +0200

    Add abstract class Dao

---
+++ models/dao/Dao.java
@@ -0,0 +1,31 @@
+package models.dao;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public abstract class Dao<T> implements ItemsIterator {
+
+    private ArrayList<T> itemsList = new ArrayList<T>();
+
+    public void add(T item) {
+        this.itemsList.add(item);
+    }
+
+    public void remove(Integer ID) {
+        Iterator iter = this.getIterator();
+
+        while (iter.hasNext()) {
+            if (iter.next().getID().equals(ID)) {
+                iter.remove();
+            }
+        }
+    }
+
+    public Iterator getIterator() {
+        return this.itemsList.iterator();
+    }
+
+    public abstract void read();
+
+    public abstract void save();
+}
