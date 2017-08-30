eafbbe049a5cd79040f9426e3a53436dbc0340fd
domakdomak <domagalski.mt@gmail.com>
2017-08-30 15:22:49 +0200

    Add CreepyGuyModel with constructor

---
+++ Models/CreepyGuyModel.java
@@ -0,0 +1,8 @@
+public class CreepyGuyModel extends UserModel {
+
+    public CreepyGuyModel(String name, String surname,
+                        String login, String password, String email) {
+
+        super(name, surname, login, password, email);
+    }
+}
