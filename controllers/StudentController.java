afd453712cf6ccc81ee09f3de538a0c64722da60
domakdomak <domagalski.mt@gmail.com>
2017-08-31 17:48:19 +0200

    Add runController in StudentController

--- controllers/StudentController.java
+++ controllers/StudentController.java
@@ -1,6 +1,31 @@
 public class StudentController {
 
-    public buyArtifact() {
+    public void runController(StudentModel student, WalletsDao walletsDao) {
+        int choice = -1;
+        final int EXIT = 0;
+
+        while(choice != EXIT){
+            StudentView.showMenu();
+            choice = chooseOption();
+
+            if(choice == 1){
+                showWallet(student, walletsDao);
+            } else if(choice == 2){
+                enterStore();
+            } else if(choice == 3){
+                showLevel(student);
+            } else if(choice == 4){
+                markStudentDoneQuest();
+            } else if(choice == 5){
+                markStudentUsedArtifact();
+            } else if(choice == 6){
+                seeStudentsWallets();
+            } else if(choice != 9){
+                MentorView.showInputError();
+            }
+        }
+    }
+
 
     }
 
