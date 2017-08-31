e897469f7949d9b98125c26cc37608e9e3081436
Kamil Postrożny <kamilp009@gmail.com>
2017-08-31 13:35:45 +0200

    Add DoneQuestModel

--- models/DoneQuestModel.java
+++ models/DoneQuestModel.java
@@ -1,1 +1,24 @@
 package models;
+
+public class DoneQuestModel {
+
+    private QuestModel quest;
+    private Integer timesDone;
+
+    public DoneQuestModel(QuestModel quest) {
+        this.quest = quest;
+        this.timesDone = 1;
+    }
+
+    public void increaseTimesDone() {
+        this.timesDone += 1;
+    }
+
+    public QuestModel getQuest() {
+        return this.quest;
+    }
+
+    public Integer getTimesDone() {
+        return this.timesDone;
+    }
+}
