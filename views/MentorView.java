0ca4a9af4d7ebbccfedc9f5f6ecd5f670531cb18
pfolus <imjanczalover18@gmail.com>
2017-08-31 15:08:14 +0200

    AddAdd MentorView class with constructor

---
+++ views/MentorView.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.lang.Integer;
+
+public class MentorView {
+
+    private static Scanner scan;
+
+    public static int getIntInput() {
+        int integer = 0;
+        boolean correctInput = false;
+
+        while(!correctInput) {
+            try {
+                scan = new Scanner(System.in);
+                System.out.println("enter integer");
+                integer = scan.nextInt();
+                correctInput = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Wrong! enter integer");
+                correctInput = false;
+            }
+        }
+        return integer;
+   }
