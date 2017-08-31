92e874c825067b6e93b58b24576210dbcfcdbfca
domakdomak <domagalski.mt@gmail.com>
2017-08-31 13:44:09 +0200

    Add constructor in StudentModel

--- models/StudentModel.java
+++ models/StudentModel.java
@@ -1,11 +1,30 @@
 package models;
 
+import java.util.ArrayList;
+
 
 public class StudentModel extends UserModel {
 
-    public StudentModel(String name, String surname,
-                        String login, String password, String email) {
+    String level;
+    Integer score;
+    WalletModel wallet;
+    ArrayList<DoneQuestModel> doneQuests;
+    ArrayList<BoughtArtifactModel> boughtArtifacts;
+    ArrayList<OrderWithTeammatesModel> ordersWithTeammates;
+
+    public StudentModel(String name,
+                        String surname, String login,
+                        String password, String email,
+                        String level, Integer score) {
 
         super(name, surname, login, password, email);
+        this.level = level;
+        this.score = score;
+        this.wallet = null;
+        this.doneQuests = null;
+        this.boughtArtifacts = null;
+        this.ordersWithTeammates = null;
+    }
+
     }
 }
