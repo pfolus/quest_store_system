ec942721ec71b7d394b4ad5a48625dd01b8a14c6
Kamil Postrożny <kamilp009@gmail.com>
2017-08-31 11:48:05 +0200

    Rename Class to ClassModel

--- models/ClassModel.java
+++ models/ClassModel.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 
-public class Class{
+public class ClassModel {
 
     private ArrayList<MentorModel> mentorsList;
     private ArrayList<StudentModel> studentsList;
