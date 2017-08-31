f3a0ee84f8dfe7dadbaf4968c4f8466c32742b4b
Kamil Postrożny <kamilp009@gmail.com>
2017-08-31 11:07:50 +0200

    Add QuestCategoriesDao

---
+++ models/dao/QuestCategoriesDao.java
@@ -0,0 +1,10 @@
+package models.dao;
+
+import models.QuestCategoryModel;
+
+public class QuestCategoriesDao extends Dao<QuestCategoryModel> {
+
+    public void read() {}
+
+    public void save() {}
+}
