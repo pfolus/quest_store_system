54ce9777f0b950c258e6810c66513079090bc45d
pfolus <imjanczalover18@gmail.com>
2017-08-31 15:09:49 +0200

    Add run method in MentorController class

--- controllers/MentorController.java
+++ controllers/MentorController.java
@@ -9,7 +9,31 @@ import java.util.InputMismatchException;
 
 public class MentorController {
 
-    public createStudent() {
+    public void run(StudentsDao studentsDao) {
+        int choice = 0;
+        final int EXIT = 9;
+
+        while(choice != EXIT){
+            MentorView.showMenu();
+            choice = MentorView.getIntInput();
+
+            if(choice == 1){
+                createStudent(studentsDao);
+            } else if(choice == 2){
+                addQuest();
+            } else if(choice == 3){
+                addArtifact();
+            } else if(choice == 4){
+                markStudentDoneQuest();
+            } else if(choice == 5){
+                markStudentUsedArtifact();
+            } else if(choice == 6){
+                seeStudentsWallets();
+            } else if(choice != 9){
+                MentorView.showInputError();
+            }
+        }
+    }
 
     }
 
