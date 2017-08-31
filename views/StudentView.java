c68edc2bd6e8415da2f07c552af90d904b4819f5
domakdomak <domagalski.mt@gmail.com>
2017-08-31 17:49:54 +0200

    Add showMenu in Studentview

--- views/StudentView.java
+++ views/StudentView.java
@@ -1,5 +1,18 @@
 public class StudentView {
 
+    public static void showMenu() {
+        String[] menuList = {"1. See My Wallet",
+                             "2. Enter Artifacts Store",
+                             "3. See My Experience Level",
+                             "4. See Artifacts I Bought",
+                             "5. See Quests I Have Finished",
+                             "0. Exit"};
+
+        for (String item : menuList) {
+            System.out.print(item);
+        }
+    }
+
     public static void showCoinsBalance(Integer balance) {
         System.out.println("You have " + balance + " coolcoins.");
     }
