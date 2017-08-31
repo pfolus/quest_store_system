928676db31987522e7f366998edd07d9fd58e344
domakdomak <domagalski.mt@gmail.com>
2017-08-31 18:21:05 +0200

    Fix ArtifactCategoryModel constructor

--- models/ArtifactCategoryModel.java
+++ models/ArtifactCategoryModel.java
@@ -2,7 +2,7 @@ package models;
 
 public class ArtifactCategoryModel extends CategoryModel {
 
-    public ArtifactCategory(String name, String id) {
-        super(name, id);
+    public ArtifactCategoryModel(String name) {
+        super(name);
     }
 }
