2dd0ad6566b2388b849234d8e9129e48183d9869
pfolus <imjanczalover18@gmail.com>
2017-08-31 15:09:13 +0200

    Add showMenu method in MentorView class

--- views/MentorView.java
+++ views/MentorView.java
@@ -30,3 +30,16 @@ public class MentorView {
    public static void showInputError() {
        System.out.println("Wrong option provided, try again!");
    }
+
+   public static void showMenu() {
+       String[] menuList = {"1.create Student", "2.add Quest", "3. add Artifact",
+                "4. mark Students Done Quests", "5. mark Students Used Artifacts",
+                "6. see Students Wallet"};
+
+       for (String item : menuList) {
+           System.out.print(item);
+       }
+   }
+
+
+}
