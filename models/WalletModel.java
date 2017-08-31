d0947ddf34d6af265d83fe7f29529515b46b1fe5
domakdomak <domagalski.mt@gmail.com>
2017-08-31 15:42:50 +0200

    Add WalletModel with constructor

--- models/WalletModel.java
+++ models/WalletModel.java
@@ -1,1 +1,16 @@
 package models;
+
+
+public class WalletModel {
+
+    private Integer balance;
+    private Integer id;
+    private Integer studentId;
+
+    private static Integer nextId = 1;
+
+    public WalletModel(Integer balance, Integer studentId) {
+        this.balance = balance;
+        this.studentId = studentId;
+        this.id = nextId++;
+    }
