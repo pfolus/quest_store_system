190da09e9077bd547e702cef545fc51eaa763d19
Kamil Postrożny <kamilp009@gmail.com>
2017-09-01 09:22:38 +0200

    Add info about login and password entering

--- views/MainView.java
+++ views/MainView.java
@@ -7,10 +7,14 @@ public class MainView {
     private static Scanner scanner = new Scanner(System.in);
 
     public static String getLogin() {
+        System.out.print("\nEnter login: ");
+
         return scanner.nextLine();
     }
 
     public static String getPassword() {
+        System.out.print("\nEnter password: ");
+
         return scanner.nextLine();
     }
 }
