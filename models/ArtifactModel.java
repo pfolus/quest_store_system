76935278909f79f04d4cc7e5ab43ab0e3ccbfb00
pfolus <imjanczalover18@gmail.com>
2017-09-01 11:25:30 +0200

    Add description attribute in AtrifactModel class

--- models/ArtifactModel.java
+++ models/ArtifactModel.java
@@ -3,10 +3,12 @@ package models;
 public class ArtifactModel extends AbstractItemModel<ArtifactCategoryModel> {
 
     private Integer price;
+    private String description;
 
-    public ArtifactModel(String name, ArtifactCategoryModel category, Integer price) {
+    public ArtifactModel(String name, String description, ArtifactCategoryModel category, Integer price) {
         super(name, category);
         this.price = price;
+        this.description = description;
     }
 
     public Integer getPrice() {
