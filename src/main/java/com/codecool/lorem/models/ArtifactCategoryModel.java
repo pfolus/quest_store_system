06c3581c443ddba47b727308fa3c4da8c5487afd
domakdomak <domagalski.mt@gmail.com>
2017-09-20 16:36:16 +0200

    Fix constructor in ArtifactCategoryModel

--- src/main/java/com/codecool/lorem/models/ArtifactCategoryModel.java
+++ src/main/java/com/codecool/lorem/models/ArtifactCategoryModel.java
@@ -2,7 +2,7 @@ package com.codecool.lorem.models;
 
 public class ArtifactCategoryModel extends CategoryModel {
 
-    public ArtifactCategoryModel(String name) {
-        super(name);
+    public ArtifactCategoryModel(String name, Integer id) {
+        super(name, id);
     }
 }
