b944c3520d203b64da4b5a4096f34d556fb56bab
Kamil Postrożny <kamilp009@gmail.com>
2017-09-20 16:11:37 +0200

    Fix MentorModel constructor

--- src/main/java/com/codecool/lorem/models/MentorModel.java
+++ src/main/java/com/codecool/lorem/models/MentorModel.java
@@ -2,10 +2,13 @@ package com.codecool.lorem.models;
 
 public class MentorModel extends UserModel {
 
-    public MentorModel(String name, String surname,
-                        String login, String password, String email) {
+    private Integer classId;
 
-        super(name, surname, login, password, email);
+    public MentorModel(Integer id, String name, String surname, String login,
+                       String password, String email, Integer classId) {
+
+        super(id, name, surname, login, password, email);
+        this.classId = classId;
     }
 
 
