c022d8f1b164861e0e501ded41ecd8ddc413b586
pfolus <imjanczalover18@gmail.com>
2017-09-21 00:15:30 +0200

    Add addQuestToDatabase and loadQuestsFromDatabase methods in QuestsDao

--- src/main/java/com/codecool/lorem/dao/QuestsDao.java
+++ src/main/java/com/codecool/lorem/dao/QuestsDao.java
@@ -1,8 +1,10 @@
 package com.codecool.lorem.dao;
 
-import java.util.Iterator;
+import java.sql.*;
 
 import com.codecool.lorem.models.QuestModel;
+import com.codecool.lorem.models.StudentModel;
+import com.codecool.lorem.views.MainView;
 
 public class QuestsDao extends Dao<QuestModel> {
 
@@ -14,4 +16,54 @@ public class QuestsDao extends Dao<QuestModel> {
         }
         return null;
     }
+
+    public void addQuestToDatabase(String name, Integer categoryId, String description, Integer prize) {
+        Statement statement = null;
+
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:src/main/db/quest-store.db")) {
+
+            statement = connection.createStatement();
+
+            String sql = String.format(
+                    "INSERT INTO quests (name, description, quest_category_id, prize) " +
+                    "VALUES ('%s', '%s', '%d', '%d');" , name, description, categoryId, prize);
+
+            statement.executeUpdate(sql);
+
+            statement.close();
+            connection.commit();
+        } catch (SQLException e) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+
+        MainView.print("Records created successfully");
+
+    }
+
+    public void loadQuestsFromDatabase() {
+        //call that shit in the constructor
+
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:src/main/db/quest-store.db")) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM quests");
+
+        while (resultSet.next()) {
+            Integer questId = resultSet.getInt("id");
+            String name = resultSet.getString("name");
+            String description = resultSet.getString("description");
+            Integer questCategoryId = resultSet.getInt("quest_category_id");
+            Integer prize = resultSet.getInt("prize");
+
+            this.itemsList.add( new QuestModel(questId, name, questCategoryId, description, prize));
+        }
+
+        resultSet.close();
+        statement.close();
+    } catch (SQLException e) {
+        System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        System.exit(0);
+    }
+    }
+
 }
