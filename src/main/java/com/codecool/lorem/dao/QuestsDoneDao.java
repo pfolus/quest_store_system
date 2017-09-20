01553085ed1dc906f8e8e466d4d6ddedfa837467
Kamil Postrożny <kamilp009@gmail.com>
2017-09-20 17:15:43 +0200

    Remove QuestsDaone Dao iterator

--- src/main/java/com/codecool/lorem/dao/QuestsDoneDao.java
+++ src/main/java/com/codecool/lorem/dao/QuestsDoneDao.java
@@ -7,19 +7,11 @@ import com.codecool.lorem.models.DoneQuestModel;
 public class QuestsDoneDao extends Dao<DoneQuestModel> {
 
     public DoneQuestModel get(Integer id) {
-        Iterator iter = this.getIterator();
-
-        while (iter.hasNext()) {
-            DoneQuestModel user = (DoneQuestModel) iter.next();
-
-            if (user.getId().equals(id)) {
-                return user;
+        for (DoneQuestModel quest : getItems()) {
+            if (quest.getId().equals(id)) {
+                return quest;
             }
         }
         return null;
     }
-
-    public void read() {}
-
-    public void save() {}
 }
