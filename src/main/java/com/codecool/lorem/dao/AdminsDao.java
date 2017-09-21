fd989407596903548fd81f86be553a8adc272da6
Kamil Postrożny <kamilp009@gmail.com>
2017-09-21 13:57:01 +0200

    Add loading Admins from databse in AdminsDao

--- src/main/java/com/codecool/lorem/dao/AdminsDao.java
+++ src/main/java/com/codecool/lorem/dao/AdminsDao.java
@@ -2,6 +2,8 @@ package com.codecool.lorem.dao;
 
 import com.codecool.lorem.models.AdminModel;
 
+import java.sql.*;
+
 public class AdminsDao extends Dao<AdminModel> {
 
     public AdminModel getById(Integer id) {
@@ -11,5 +13,30 @@ public class AdminsDao extends Dao<AdminModel> {
             }
         }
         return null;
+    }
+
+    public void loadAdminsFromDatabase(){
+        try {
+            Connection connection = DatabaseConnection.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM users;");
+
+            while (resultSet.next()) {
+                Integer user_id = resultSet.getInt("user_id");
+                String name = resultSet.getString("name");
+                String surname = resultSet.getString("surname");
+                String login = resultSet.getString("login");
+                String password = resultSet.getString("password");
+                String email = resultSet.getString("email");
+
+                this.itemsList.add(new AdminModel(user_id, name, surname,
+                        login, password, email));
+            }
+
+            resultSet.close();
+            statement.close();
+        } catch (SQLException e) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
     }
 }
