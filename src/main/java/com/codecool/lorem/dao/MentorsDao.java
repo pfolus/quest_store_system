986dec7e1e07ff32c109be7d670278adb11b5422
pfolus <imjanczalover18@gmail.com>
2017-09-21 10:29:28 +0200

    Add createLoggedMentor method in MentorsDao class

--- src/main/java/com/codecool/lorem/dao/MentorsDao.java
+++ src/main/java/com/codecool/lorem/dao/MentorsDao.java
@@ -1,8 +1,10 @@
 package com.codecool.lorem.dao;
 
+import java.sql.*;
 import java.util.Iterator;
 
 import com.codecool.lorem.models.MentorModel;
+import com.codecool.lorem.models.StudentModel;
 
 public class MentorsDao extends Dao<MentorModel> {
 
@@ -13,6 +15,35 @@ public class MentorsDao extends Dao<MentorModel> {
             }
         }
         return null;
+    }
+
+    public StudentModel createLoggedMentor(String id) {
+        StudentModel result = null;
+
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:src/main/db/quest-store.db")) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(
+                    "SELECT * FROM users INNER JOIN mentors_data ON users.id = mentors_data.user_id");
+
+            if (resultSet.next()) {
+                Integer user_id = resultSet.getInt("user_id");
+                String name = resultSet.getString("name");
+                String surname = resultSet.getString("surname");
+                String login = resultSet.getString("login");
+                String password = resultSet.getString("password");
+                String email = resultSet.getString("email");
+                Integer classId = resultSet.getInt("class_id");
+
+                result = new MentorModel(user_id, name, surname, login, password, email, classId);
+            }
+
+            resultSet.close();
+            statement.close();
+        } catch (SQLException e) {
+            return result;
+        }
+
+        return result;
     }
 
 }
