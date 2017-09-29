8ea3974940ec228a2d6db660fc128ac1746af714
Kamil Postrożny <kamilp009@gmail.com>
2017-09-29 16:56:48 +0200

    getById moved to base class

--- src/main/java/com/codecool/lorem/models/ArtifactCategoryModel.java
+++ src/main/java/com/codecool/lorem/models/ArtifactCategoryModel.java
@@ -5,8 +5,4 @@ public class ArtifactCategoryModel extends CategoryModel {
     public ArtifactCategoryModel(Integer id, String name) {
         super(id, name);
     }
-
-    public String toString() {
-        return this.id + ". " + this.name;
-    }
 }
