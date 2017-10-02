885e81fca98bcaaacf2fe8378682573623efcec0
Kamil Postrożny <kamilp009@gmail.com>
2017-10-02 11:53:36 +0200

    Change type of return value of logging in LoginDao

--- src/main/java/com/codecool/lorem/dao/LoginDao.java
+++ src/main/java/com/codecool/lorem/dao/LoginDao.java
@@ -1,12 +1,14 @@
 package com.codecool.lorem.dao;
 
+import com.codecool.lorem.models.PendingUserModel;
+
 import java.sql.*;
 import java.util.ArrayList;
 
 public class LoginDao {
 
-    public ArrayList<String> login(String login, String password) {
-        ArrayList<String> result = null;
+    public PendingUserModel login(String login, String password) {
+        PendingUserModel pendingUser = null;
 
         try {
             Connection connection = DatabaseConnection.getConnection();
@@ -15,10 +17,8 @@ public class LoginDao {
                     "SELECT type, id FROM users WHERE login = '" + login + "' AND password = '" + password + "';");
 
             if (resultSet.next()) {
-                result = new ArrayList<>();
-
-                result.add(resultSet.getString("type"));
-                result.add(resultSet.getString("id"));
+                pendingUser = new PendingUserModel(resultSet.getString("id"),
+                        resultSet.getString("type"));
             } else {
                 throw new SQLException();
             }
@@ -29,6 +29,6 @@ public class LoginDao {
             System.out.println("\nWrong data! Couldn't login.");
         }
 
-        return result;
+        return pendingUser;
     }
 }
