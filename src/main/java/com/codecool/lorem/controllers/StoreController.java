92d73df87771d2a09f00483c56a4e1665ad9bdbd
domakdomak <domagalski.mt@gmail.com>
2017-10-02 17:01:18 +0200

    Add managePendingTransactions in StoreController

--- src/main/java/com/codecool/lorem/controllers/StoreController.java
+++ src/main/java/com/codecool/lorem/controllers/StoreController.java
@@ -3,14 +3,14 @@ package com.codecool.lorem.controllers;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.Scanner;
 
+import com.codecool.lorem.dao.*;
+import com.codecool.lorem.models.GroupTransactionModel;
 import com.codecool.lorem.views.StoreView;
 import com.codecool.lorem.models.ArtifactModel;
 import com.codecool.lorem.models.StudentModel;
 import com.codecool.lorem.models.WalletModel;
-import com.codecool.lorem.dao.ArtifactsBoughtDao;
-import com.codecool.lorem.dao.ArtifactsDao;
-import com.codecool.lorem.dao.WalletsDao;
 
 
 public class StoreController {
@@ -27,10 +27,36 @@ public class StoreController {
             if (choice == 1) {
                 buyArtifact(student, wallet);
             } else if (choice == 2) {
-                //buyArtifactWithTeammates();
+                buyArtifactWithTeammates(student, wallet);
+            } else if (choice == 3) {
+                managePendingTransactions(student);
             }
         }
     }
+
+    private static void managePendingTransactions(StudentModel student) {
+        GroupTransactionsDao transactions = new GroupTransactionsDao();
+        StoreView.showTransactions(transactions.getTransactionsByStudentId(student.getId()));
+        Integer transactionId = StoreView.chooseTransactionId();
+
+        if (!transactionId.equals(0)) {
+            GroupTransactionModel transaction = transactions.getById(transactionId);
+            Integer choice = StoreView.acceptOrDeclineTransaction(transaction);
+
+            if (choice.equals(0)) {
+                transactions.removeTransaction(transaction);
+
+            } else if (choice.equals(1)) {
+
+                if (transaction.getRequiredAcceptances().equals(transaction.getStudentsAccepted() + 1)) {
+                    //buy
+                } else {
+                    transactions.markTransaction(transaction, student);
+                }
+            }
+        }
+    }
+
 
     private static void buyArtifact(StudentModel student, WalletModel wallet) {
 
@@ -38,7 +64,7 @@ public class StoreController {
         ArtifactsDao artifacts = getArtifacts();
         ArtifactModel artifact = chooseArtifactById(artifacts);
 
-        if (hasEnoughCoins(wallet, artifact)) {
+        if (hasEnoughCoins(wallet, artifact.getPrice())) {
 
             WalletsDao walletsDao = new WalletsDao();
             wallet.reduceBalance(artifact.getPrice());
@@ -53,7 +79,7 @@ public class StoreController {
 
     private static ArtifactsDao getArtifacts() {
         ArtifactsDao artifactsDatabase = new ArtifactsDao();
-        artifactsDatabase.readFromDatabase();
+        //artifactsDatabase.readFromDatabase();
         ArrayList<ArtifactModel> artifacts = artifactsDatabase.getItems();
         StoreView.showArtifacts(artifacts);
 
@@ -67,6 +93,7 @@ public class StoreController {
     }
 
     private static ArtifactModel chooseArtifactById(ArtifactsDao artifacts) {
+        getGroupArtifacts(artifacts);
         Integer id = chooseArtifactId();
         ArtifactModel artifact = artifacts.getById(id);
 
@@ -89,12 +116,64 @@ public class StoreController {
         return id;
     }
 
-    //private static BoughtArtifactModel buyArtifactWithTeammates() {
+    private static void buyArtifactWithTeammates(StudentModel student, WalletModel wallet) {
+        ArtifactsDao artifacts = new ArtifactsDao();
+        ArtifactModel artifact = chooseArtifactById(artifacts);
 
-    //}
+        ArrayList<StudentModel> buyers = chooseStudentsToBuyAnArtifact();
+        buyers.add(student);
 
-    private static boolean hasEnoughCoins(WalletModel wallet, ArtifactModel artifactToBuy) {
-        if (wallet.getBalance() > artifactToBuy.getPrice()) {
+        Integer pricePerStudent = artifact.getPrice() / buyers.size();
+        if (hasEnoughCoins(wallet, pricePerStudent)) {
+            createGroupTransactions(buyers, artifact, pricePerStudent);
+
+        } else {
+            StoreView.notEnoughMoneyInfo();
+        }
+    }
+
+    private static void createGroupTransactions(ArrayList<StudentModel> buyers, ArtifactModel artifact, Integer pricePerStudent) {
+
+        GroupTransactionsDao transactions = new GroupTransactionsDao();
+        Integer transactionId = transactions.getNextId();
+
+        for (StudentModel s:buyers) {
+            GroupTransactionModel transaction = new GroupTransactionModel(transactionId, artifact.getId(), 1, buyers.size(), s.getId(), pricePerStudent, "Not Marked");
+            transactions.addTransactionToDatabase(transaction);
+        }
+    }
+
+    private static void getGroupArtifacts(ArtifactsDao artifacts) {
+        StoreView.showArtifacts(artifacts.getGroupArtifacts());
+    }
+
+
+    private static ArrayList<StudentModel> chooseStudentsToBuyAnArtifact() {
+
+        ArrayList<StudentModel> chosenStudents = new ArrayList<>();
+
+        StudentsDao students = new StudentsDao();
+
+        Integer id = 1000;
+        while (id != 0) {
+            StoreView.showStudents(students.getItems());
+            Scanner in = new Scanner(System.in);
+            System.out.println("Choose student id, type '0' to end: ");
+            id = in.nextInt();
+            StudentModel student = students.getById(id);
+
+            if (!(chosenStudents.contains(student)) && student != null) {
+                chosenStudents.add(student);
+
+            } else {
+                System.out.println("This student is already added!");
+            }
+        }
+        return chosenStudents;
+    }
+
+    private static boolean hasEnoughCoins(WalletModel wallet, Integer price) {
+        if (wallet.getBalance() > price) {
             return true;
         } else {
             return false;
@@ -103,7 +182,7 @@ public class StoreController {
 
     private static Integer chooseOption() {
         Integer choice = null;
-        final Integer[] CHOICES = {0, 1, 2};
+        final Integer[] CHOICES = {0, 1, 2, 3};
 
         while (!Arrays.asList(CHOICES).contains(choice)){
             try {
