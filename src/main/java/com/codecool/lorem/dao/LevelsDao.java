4e515fc19a518f12acb1d6f39671b1308c4993c4
domakdomak <domagalski.mt@gmail.com>
2017-10-02 11:23:36 +0200

    Add readFromDatabase and checkLevel to LevelsDao

--- src/main/java/com/codecool/lorem/dao/LevelsDao.java
+++ src/main/java/com/codecool/lorem/dao/LevelsDao.java
@@ -1,9 +1,53 @@
 package com.codecool.lorem.dao;
 
-import java.util.Iterator;
+import java.sql.Connection;
+import static java.util.Comparator.comparing;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collections;
 
 import com.codecool.lorem.models.LevelModel;
 
 public class LevelsDao extends Dao<LevelModel> {
 
+    public LevelsDao() {
+        readFromDatabase();
+    }
+
+    private void readFromDatabase() {
+
+        try {
+            Connection connection = DatabaseConnection.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM levels;");
+
+            while (resultSet.next()) {
+                Integer id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                Integer requiredScore = resultSet.getInt("required_score");
+
+                LevelModel level = new LevelModel(id, requiredScore, name);
+                this.itemsList.add(level);
+            }
+
+            resultSet.close();
+            statement.close();
+        } catch (SQLException e) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+    }
+
+    public String checkLevel(Integer score) {
+        String levelName = "";
+
+        Collections.sort(this.itemsList, comparing(LevelModel::getRequiredScore));
+
+        for (LevelModel level:this.itemsList) {
+            if (score >= level.getRequiredScore()) {
+                levelName = level.getName();
+            }
+        }
+        return levelName;
+    }
 }
