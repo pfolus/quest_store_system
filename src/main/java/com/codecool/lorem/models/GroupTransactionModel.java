88a59a76f23e757b79435186461bda0c313d0cb5
domakdomak <domagalski.mt@gmail.com>
2017-10-03 10:10:11 +0200

    Fix group transaction model

--- src/main/java/com/codecool/lorem/models/GroupTransactionModel.java
+++ src/main/java/com/codecool/lorem/models/GroupTransactionModel.java
@@ -3,17 +3,13 @@ package com.codecool.lorem.models;
 public class GroupTransactionModel extends AbstractItemModel {
 
     private Integer artifactId;
-    private Integer studentsAccepted;
-    private Integer requiredAcceptances;
     private Integer studentId;
     private Integer price;
     private String status;
 
-    public GroupTransactionModel(Integer id, Integer artifactId, Integer studentsAccepted, Integer requiredAcceptances, Integer studentId, Integer price, String status) {
+    public GroupTransactionModel(Integer id, Integer artifactId, Integer studentId, Integer price, String status) {
         super(id);
         this.artifactId = artifactId;
-        this.studentsAccepted = studentsAccepted;
-        this.requiredAcceptances = requiredAcceptances;
         this.studentId = studentId;
         this.price = price;
         this.status = status;
@@ -21,14 +17,6 @@ public class GroupTransactionModel extends AbstractItemModel {
 
     public Integer getArtifactId() {
         return this.artifactId;
-    }
-
-    public Integer getStudentsAccepted() {
-        return this.studentsAccepted;
-    }
-
-    public Integer getRequiredAcceptances() {
-        return this.requiredAcceptances;
     }
 
     public Integer getStudentId() {
@@ -43,15 +31,10 @@ public class GroupTransactionModel extends AbstractItemModel {
         return this.status;
     }
 
-    public void raiseStudentsAccepted() {
-        this.studentsAccepted += 1;
-    }
 
     public String toString() {
         return  "ID: " + this.id +
                 " Artifact ID: " + artifactId +
-                " Accepted: " + studentsAccepted +
-                " Required: " + requiredAcceptances +
                 " Price: " + price;
     }
 
