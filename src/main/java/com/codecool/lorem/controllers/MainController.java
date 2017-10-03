cdc648dc67fed2d393dcee86f2b9216091acc560
Kamil Postrożny <kamilp009@gmail.com>
2017-10-03 10:10:10 +0200

    Change PendingUserModel to PendingUser

--- src/main/java/com/codecool/lorem/controllers/MainController.java
+++ src/main/java/com/codecool/lorem/controllers/MainController.java
@@ -1,20 +1,20 @@
 package com.codecool.lorem.controllers;
 
 import com.codecool.lorem.dao.*;
-import com.codecool.lorem.models.PendingUserModel;
+import com.codecool.lorem.models.PendingUser;
 import com.codecool.lorem.views.MainView;
 
 public class MainController {
 
     public static void runController() {
-        PendingUserModel pendingUser = login();
+        PendingUser pendingUser = login();
         chooseController(pendingUser);
         DatabaseConnection.closeConnection();
     }
 
-    private static PendingUserModel login() {
+    private static PendingUser login() {
         LoginDao loginDao = new LoginDao();
-        PendingUserModel pendingUser = null;
+        PendingUser pendingUser = null;
         String login;
         String password;
 
@@ -27,19 +27,20 @@ public class MainController {
         return pendingUser;
     }
 
-    private static void chooseController(PendingUserModel pendingUser) {
-        String id = pendingUser.getId();
+    private static void chooseController(PendingUser pendingUser) {
+        Integer id = pendingUser.getId();
         String type = pendingUser.getType();
 
         if (type.equals("student")) {
             StudentsDao studentsDao = new StudentsDao();
-            StudentController.runController(studentsDao.createLoggedStudent(id));
+            StudentController.runController(studentsDao.createLoggedStudent(String.valueOf(id)));
         } else if (type.equals("mentor")) {
             MentorsDao mentorsDao = new MentorsDao();
-            MentorController.runController(mentorsDao.createLoggedMentor(id));
+            MentorController.runController(mentorsDao.getById(id));
         } else if (type.equals("admin")) {
             AdminsDao adminsDao = new AdminsDao();
-            AdminController.runController(adminsDao.createLoggedAdmin(id));
+            AdminController adminController = new AdminController(adminsDao.getById(id));
+            adminController.runController();
         }
     }
 }
