e9cbb49a4d361625565225b9a1e28da0cb6f8e8b
jaguuuar <sirkamson@gmail.com>
2017-10-04 16:15:32 +0200

    Refactor tests

--- src/test/java/com/codecool/lorem/models/AdminModelTest.java
+++ src/test/java/com/codecool/lorem/models/AdminModelTest.java
@@ -6,55 +6,41 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AdminModelTest {
 
+    private AdminModel admin = new AdminModel(2, "Bartek", "Kowal", "kowal123",
+            "kowal321",  "kowal@gmail.com");
+
     @Test
     public void testIsIdNotNull() {
-        AdminModel admin = new AdminModel(2, "Bartek", "Kowal", "kowal123",
-                "kowal321",  "kowal@gmail.com");
         assertNotNull(admin.getId());
     }
 
-
     @Test
     public void testIsNameIsNotNull() {
-        AdminModel admin = new AdminModel(2, "Bartek", "Kowal", "kowal123",
-                                          "kowal321",  "kowal@gmail.com");
         assertNotNull(admin.getName());
     }
 
     @Test
     public void testIsSurnameIsNotNull() {
-        AdminModel admin = new AdminModel(2, "Bartek", "Kowal", "kowal123",
-                "kowal321",  "kowal@gmail.com");
         assertNotNull(admin.getSurname());
     }
 
     @Test
     public void testIsLoginIsNotNull() {
-        AdminModel admin = new AdminModel(2, "Bartek", "Kowal", "kowal123",
-                "kowal321",  "kowal@gmail.com");
         assertNotNull(admin.getLogin());
     }
 
     @Test
     public void testIsPasswordIsNotNull() {
-        AdminModel admin = new AdminModel(2, "Bartek", "Kowal", "kowal123",
-                "kowal321",  "kowal@gmail.com");
         assertNotNull(admin.getPassword());
     }
 
     @Test
     public void testIsEmailIsNotNull() {
-        AdminModel admin = new AdminModel(2, "Bartek", "Kowal", "kowal123",
-                "kowal321",  "kowal@gmail.com");
         assertNotNull(admin.getEmail());
     }
 
     @Test
     public void testIsFullNameIsNotNull() {
-        AdminModel admin = new AdminModel(2, "Bartek", "Kowal", "kowal123",
-                "kowal321",  "kowal@gmail.com");
         assertNotNull(admin.getFullName());
     }
-
-
 }
---
+++ src/test/java/com/codecool/lorem/models/QuestCategoryModelTest.java
@@ -0,0 +1,21 @@
+package com.codecool.lorem.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestCategoryModelTest {
+
+    QuestCategoryModel questCategory = new QuestCategoryModel(2, "sport");
+
+    @Test
+    public void testIsNameIsNotNull() {
+        assertNotNull(questCategory.getName());
+    }
+
+    @Test
+    public void testToString() {
+        String expected = "2. sport";
+        assertEquals(expected , questCategory.toString());
+    }
+}
