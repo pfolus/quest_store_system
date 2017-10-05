e3b2235a3da63c1660640a82e124af9d1725dd80
szmytke <szmytke@gmail.com>
2017-10-05 11:09:20 +0200

    Change assertNotNull to assertEquals, add @BeforeEach, delete @Nested.

--- src/test/java/com/codecool/lorem/models/GroupTransactionModelTest.java
+++ src/test/java/com/codecool/lorem/models/GroupTransactionModelTest.java
@@ -1,48 +1,52 @@
 package com.codecool.lorem.models;
 
-import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GroupTransactionModelTest {
 
-    private GroupTransactionModel groupTransactionModel = new GroupTransactionModel(2,12,5,
-                                                                            300,"done");
+    private GroupTransactionModel groupTransactionModel;
 
+    @BeforeEach
+    void initGroupTransactionModel() {
+        this.groupTransactionModel = new GroupTransactionModel(2,12,5,
+                                300,"done");
+    }
     @Test
-    void testIsArtifactIdNotNull() {
-        assertNotNull(groupTransactionModel.getArtifactId());
+
+    void testGetArtifactId() {
+        Integer expected = 12;
+        assertEquals(expected, this.groupTransactionModel.getArtifactId());
     }
 
     @Test
-    void testIsStudentIdNotNull() {
-        assertNotNull(groupTransactionModel.getStudentId());
+    void testGetStudentId() {
+        Integer expected = 5;
+        assertEquals(expected, this.groupTransactionModel.getStudentId());
     }
 
     @Test
-    void testIsPriceNotNull() {
-        assertNotNull(groupTransactionModel.getPrice());
+    void testGetPrice() {
+        Integer expected = 300;
+        assertEquals(expected, this.groupTransactionModel.getPrice());
     }
 
-    @Nested
-    class StatusFieldTests {
+    @Test
+    void testGetStatus() {
+        assertEquals("done", this.groupTransactionModel.getStatus());
+    }
 
-        @Test
-        void testIsStatusIsNotNull() {
-            assertNotNull(groupTransactionModel.getStatus());
-        }
-
-        @Test
-        void testSetStatus() {
-            groupTransactionModel.setStatus();
-            assertEquals("Marked", groupTransactionModel.getStatus());
-        }
+    @Test
+    void testSetStatus() {
+        this.groupTransactionModel.setStatus();
+        assertEquals("Marked", this.groupTransactionModel.getStatus());
     }
 
     @Test
     void testToSting() {
         String expected = "ID: 2 Artifact ID: 12 Price: 300";
-        assertEquals(expected, groupTransactionModel.toString());
+        assertEquals(expected, this.groupTransactionModel.toString());
     }
 }
