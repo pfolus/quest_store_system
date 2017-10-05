a058e552c7b40a672e8b64a02a945bece80aa09c
szmytke <szmytke@gmail.com>
2017-10-05 10:23:52 +0200

    Add testToString().

--- src/test/java/com/codecool/lorem/models/ArtifactCategoryModelTest.java
+++ src/test/java/com/codecool/lorem/models/ArtifactCategoryModelTest.java
@@ -1,16 +1,21 @@
 package com.codecool.lorem.models;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ArtifactCategoryModelTest {
 
     private ArtifactCategoryModel artifactCategory = new ArtifactCategoryModel(1,"Private Mentoring");
+    
+    @Test
+    public void testGetName() {
+        assertEquals("Private Mentoring", artifactCategory.getName());
+    }
 
     @Test
-    public void testName() {
-        String expected = "Private Mentoring";
-        assertEquals(expected, artifactCategory.getName());
+    public void testToString() {
+        assertEquals("1. Private Mentoring" , this.artifactCategory.toString());
     }
 }
